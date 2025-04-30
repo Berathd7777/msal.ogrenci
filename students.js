2d52530bb9f639b25d69c2a7cb7a2de015aa26b9
Berathd7777 <hasanberatkaylan@gmail.com>
2025-04-30 08:46:53 +0300

    Update students.js

--- students.js
+++ students.js
@@ -1,7 +1,10 @@
 const students = [
     { no: 6018, name: "Ramazan Miran İlhan", gender: "Erkek" },
+{ no: 6231, name: "Tırtarlı Erdem", gender: "Erkek" },
     { no: 6205, name: "Kadir Cahit Alcan", gender: "Erkek" },
     { no: 6209, name: "Köksal Buluklu", gender: "Erkek" },
+    { no: 6211, name: "Yasin Kılıçarslan", gender: "Erkek" },
+    { no: 6211, name: "Yasin Kılıçarslan", gender: "Erkek" },
     { no: 6211, name: "Yasin Kılıçarslan", gender: "Erkek" },
     { no: 6212, name: "Celal Çelik", gender: "Erkek" },
     { no: 6214, name: "Mehmet Emin Onat", gender: "Erkek" },
@@ -9,7 +12,11 @@ const students = [
     { no: 6220, name: "Ahmet Kaya", gender: "Erkek" },
     { no: 6224, name: "Ahmed Feridun Güneş", gender: "Erkek" },
     { no: 6226, name: "Çağan Gökgündüz", gender: "Erkek" },
+    { no: 6211, name: "Yasin Kılıçarslan", gender: "Erkek" },
+    { no: 6211, name: "Yasin Kılıçarslan", gender: "Erkek" },
     { no: 6229, name: "Emir Turan Erdoğan", gender: "Erkek" },
     { no: 6231, name: "Tırtarlı Erdem", gender: "Erkek" },
     { no: 6232, name: "Emirhan Ayhan", gender: "Erkek" }
+{ no: 6231, name: "Tırtarlı Erdem", gender: "Erkek" },
+{ no: 0000, name: "Mirket", gender: "Erkek" },
 ];
